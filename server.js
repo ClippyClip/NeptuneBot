820117d642c8c2944652a8a8a2e8957bee94e3d9
ClippyClip <41496373+ClippyClip@users.noreply.github.com>
2018-07-21 16:19:43 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//Constant Variables
+const prefix = 'n!';
+const ownerID = '81829298129866752';
+
+//Listener Events
+client.on('message', message => {
+
+    //Variables
+    let args = message.content.slice(prefix.length).trim().split(' ');
+    let cmd = args.shift().toLowerCase();
+
+    //Return Statements
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    //Command Handler
+    try {
+
+        let commandFile = require(`./commands/${cmd}.js`);
+        commandFile.run(client, message, args);
+
+    } catch (e) {
+        console.log(e.stack);
+    });
+
+//Ready Event
+client.on('ready', () => console.log('Launched!'));
+
+//Discord Login
+client.login(process.env.NDcwMDA4MTY3ODI5NDcxMjQy.DjTd4A.3N53nUi - OKgvAaV2VwpabnPswn4);
